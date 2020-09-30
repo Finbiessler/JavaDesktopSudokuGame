6bcf71ac4dd977e9a5fe94178eba03a08c313d71
Fin Bießler <fin.biess@gmx.de>
2020-09-30 17:07:43 +0200

    Commented GameLogic class

--- src/sudoku/computationlogic/GameLogic.java
+++ src/sudoku/computationlogic/GameLogic.java
@@ -6,7 +6,11 @@ import sudoku.problemdomain.SudokuGame;
 
 import java.util.*;
 
+// Class regarding for checking the sudoku rules
+// and other game logic related functionalities
 public class GameLogic {
+
+    // returns a new game which can be solved but is incomplete
     public static SudokuGame getNewGame(){
         return new SudokuGame(
                 GameState.NEW,
@@ -14,18 +18,24 @@ public class GameLogic {
         );
     }
 
+
+    // Checks whether the game is complete i.e.
+    // game is valid and all tiles are filled
     public static GameState checkForCompletion(int[][] grid){
         if(sudokuIsInvalid(grid)) return GameState.ACTIVE;
         if(tilesAreNotFilled(grid)) return GameState.ACTIVE;
         return GameState.COMPLETE;
     }
 
+    // Checks whether the sudoku is invalid
+    // by checking if the rows, columns or squares of a game are invalid
     public static boolean sudokuIsInvalid(int[][] grid) {
         if(rowsAreInvalid(grid)) return true;
         if(columnsAreInvalid(grid)) return true;
         return squaresAreInvalid(grid);
     }
 
+    // Checks wheter there is any invalid row in the game
     private static boolean rowsAreInvalid(int[][] grid) {
         for(int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++){
             List<Integer> row = new ArrayList<>();
@@ -39,7 +49,7 @@ public class GameLogic {
         return false;
     }
 
-
+    // Checks wheter there is any invalid column in the game
     private static boolean columnsAreInvalid(int[][] grid) {
         for(int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++){
             List<Integer> column = new ArrayList<>();
@@ -53,15 +63,19 @@ public class GameLogic {
         return false;
     }
 
+    // Checks whether there is a invalid square in the game
+    // By checking each row of squares (top, middle,bottom) independently
     private static boolean squaresAreInvalid(int[][] grid) {
 
         if(rowOfSquaresIsInvalid(Rows.TOP, grid)) return true;
 
-        if(rowOfSquaresIsInvalid(Rows.TOP, grid)) return true;
+        if(rowOfSquaresIsInvalid(Rows.MIDDLE, grid)) return true;
 
-        return rowOfSquaresIsInvalid(Rows.TOP, grid);
+        return rowOfSquaresIsInvalid(Rows.BOTTOM, grid);
     }
 
+    // Checks whether one row of square invalid
+    // by checking each square in the certain row
     private static boolean rowOfSquaresIsInvalid(Rows value, int[][] grid) {
         switch(value){
             case TOP:
@@ -81,7 +95,8 @@ public class GameLogic {
         }
     }
 
-    //reverse y and x
+    // error: reverse y and x
+    // Checks whether a square is valid
     private static boolean squareIsInvalid(int yIndex, int xIndex, int[][] grid) {
         int xIndexEnd = xIndex + 3;
         int yIndexEnd = yIndex + 3;
@@ -99,6 +114,8 @@ public class GameLogic {
         return collectionHasRepeats(square);
     }
 
+    //error: other impl
+    // Checks whether a collection has duplicates
     public static boolean collectionHasRepeats(List<Integer> square) {
         Set<Integer> duplicates = new LinkedHashSet<>();
         Set<Integer> uniques = new HashSet<>();
@@ -113,6 +130,7 @@ public class GameLogic {
         return duplicates.size() != 0;
     }
 
+    // Checks whether all tiles of the game are filled
     public static boolean tilesAreNotFilled(int[][] grid) {
         for(int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++){
             for(int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++){
